5d34d426bac88c07d7abd0d438caba6168e1878c
sukalyan Adhikary <sadhikary0055@gmail.com>
2024-01-25 12:19:52 +0530

    push by sukalyan

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
+import Home from "./components/Home";
 
 
 function App() {
   return (
+    <>
   <h1 className="text-3xl font-bold underline bg-red-600  ">
     Hello world!
 
     Lets start guys
+
+    
   </h1>
+  <Home/>
+  </>
+  
 
   );
 }
