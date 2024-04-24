4abcbfd19498126a3a524b2b95a443770d1bf436
Pabitra <pm.pabitra.mondal.2003@gmail.com>
2024-04-25 00:56:02 +0530

    supper

--- backend/routers/super.router.js
+++ backend/routers/super.router.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { teacherRequest } from "../controllers/super.controller.js";
+import { teacherRequest, teacherDelete } from "../controllers/super.controller.js";
 const router = express.Router();
 
 teacherRequest
 
 router.get("/teacherRequest", teacherRequest);
+router.delete("/teacherDelete/:id", teacherDelete);
 
 
 export default router
