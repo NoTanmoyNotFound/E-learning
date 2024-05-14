da32e6d827642ef4f3e1028cb072135baac99430
artic0909 <alfag327@gmail.com>
2024-05-14 21:35:11 +0530

    fr & backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,6 @@ import ProfileView from "./components/Profile/ProfileView/ProfileView";
 //course routes start
 import AllCourses from "./components/AllCourses/AllCourses";
 import SingleCourseDetails from "./components/AllCourses/SingleCourseDetails/SingleCourseDetails";
-import MainCourse from "./components/AllCourses/MainCourse/MainCourse";
 import Main_Course2 from "./components/AllCourses/MainCourse/Main_Course2";
 //course routes end
 
@@ -72,8 +71,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/allCourses" element={<AllCourses />} />
         <Route path="/course-details/:courseId" element={<SingleCourseDetails />} />
-        <Route path="/MainCourse/:courseId" element={<MainCourse />} />
-        <Route path="/Main_Course2" element={<Main_Course2 />} />
+        <Route path="/MainCourse/:courseId" element={<Main_Course2 />} />
 
 
 
